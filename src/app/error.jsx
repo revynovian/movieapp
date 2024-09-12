5ec69c0564c512956cee535394dd665d49ba4004
revynovian <revy.nw@gmail.com>
2024-09-12 15:14:58 +0700

    add loading/spinner

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 export default function error({error, reset}) {
   useEffect(()=>{
     console.log(error)
-  })
+  },[error])
   return (
     <div className="text-center mt-10">
       <h1>Something went wrong, Please try again later</h1>
---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className="flex justify-center mt-16">
+      <img className="h-20" src="spinner.svg" alt="loading" />
+    </div>
+  )
+}
