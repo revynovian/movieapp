cfd1a92afb2b71da0cac5a6374968e054f8e4548
revynovian <revy.nw@gmail.com>
2024-09-12 12:55:30 +0700

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,28 @@
+export default function () {
+  return (
+    <div className="mx-auto max-w-6xl gap-4 space-y-4 p-3">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Layar Info is your go-to platform for everything related to movies, TV
+        shows, and entertainment. Whether you're a fan of blockbusters, indie
+        films, or binge-worthy series, our extensive database has something for
+        everyone. Explore detailed profiles of actors, directors, and crew
+        members, alongside comprehensive information on the latest releases,
+        reviews, and trailers. Layar Info makes it easy to discover new
+        favorites, stay updated on what's trending, and dive deep into the
+        behind-the-scenes world of entertainment.
+      </p>
+      <p>
+        Built for passionate viewers and industry insiders alike, Layar Info
+        aims to be a reliable source for all things cinematic. Our user-friendly
+        interface and powerful search tools allow you to quickly access
+        information on your favorite titles, past and present. With features
+        like ratings, recommendations, and personalized watchlists, you can
+        curate your entertainment experience with ease. Whether you're looking
+        to learn more about the newest blockbuster or want to revisit a classic,
+        Layar Info is here to guide you through the ever-evolving world of film
+        and television.
+      </p>
+    </div>
+  );
+}
