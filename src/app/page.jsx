46498afd9cd015c528d9650a35f4c7cf2d053a2e
revynovian <revy.nw@gmail.com>
2024-09-14 00:26:50 +0700

    fix minor bugs 2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
 
-export default async function Home({}) {
+export default function Home({}) {
    
   return (
     <div className="sm:h-[80vh] flex justify-center items-center">
